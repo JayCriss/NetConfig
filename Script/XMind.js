42dead3f5fbfcbbc06a747f2c0dd00e49f8af5a3
JayCriss <60795758+JayCriss@users.noreply.github.com>
2020-07-04 11:53:14 +0800

    XMind Pro Unlocked

---
+++ Script/XMind.js
@@ -0,0 +1,24 @@
+/* **************************
+ * XMind Pro Unlocked
+ * *************************
+【 QX 1.0.5+ 脚本配置 】 :
+*************************
+[rewrite_local]
+^https:\/\/www\.xmind\.cn\/\_res\/devices url script-response-body QX4m/XMind.js
+
+[mitm]
+hostname = www.xmind.cn
+*************************/
+
+var obj = JSON.parse($response.body);
+
+obj = {
+   "raw_data": "DBcBHgojrvPgruIJMfb4KK/76CmjxSHSp9KipRwOnQUuf+Gt2FncFUzNvxZydUeXEzDZt/XWEm91lHFYrvT0f2AFap2L4psLI/34sKU9VLGH7kFsBYlexU/nifBtosMJqQxL4TU1pjvjl+1uyWsFAeGR42aEnVhQWgvJB5Ovcd0=",
+   "license": {
+     "status": "sub",
+     "expireTime": 3070928235000
+   },
+   "_code": 200
+ }
+
+$done({body: JSON.stringify(obj)});
