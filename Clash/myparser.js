173647ba87617f08ff0da885f10bfc4eee743b75
JayCriss <60795758+JayCriss@users.noreply.github.com>
2020-11-30 23:47:21 +0800

    Create myparser.js

---
+++ Clash/myparser.js
@@ -0,0 +1,22 @@
+module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url, interval, selected }) => {
+    const obj = yaml.parse(raw);
+    const orig = ['香港','台湾','日本','韩国', //0~3
+                  '新加坡','美国','加拿大','德国', //4~7
+                  '英国','澳洲'];
+    const goal = ['🇭🇰 香港','🇹🇼 台湾','🇯🇵 日本','🇰🇷 韩国',
+                  '🇸🇬 新加坡','🇺🇸 美国','🇨🇦 加拿大','🇩🇪 德国',
+                  '🇬🇧 英国','🇦🇺 澳洲'];
+    // 很奇怪，让goal循环放外层才能正确匹配完整
+    for (let j = 0; j < goal.length; j++) {
+        for (let i = 0; i < obj.proxies.length; i++) {
+            let proxyName = obj.proxies[i].name;
+            if (proxyName.search(orig[j] >= 0)) {
+                let temp_str = proxyName.replace(orig[j], goal[j]);
+                obj.proxies[i].name = temp_str;
+                obj['proxy-groups'][0].proxies[i] = temp_str;
+            }
+        }
+    }
+
+    return yaml.stringify(obj)
+  }
