2e6af68e1fce0baaa9a787b7e86dcdcd65bf56fe
JayCriss <60795758+JayCriss@users.noreply.github.com>
2021-05-23 20:59:42 +0800

    Update myparser.js

--- Clash/myparser.js
+++ Clash/myparser.js
@@ -6,22 +6,30 @@ parsers: # array
 
 // Clash for Windows parser
 const fs = require('fs'); //支持用npm的包
+const sortBar = ['🇹🇼', '🇯🇵', '🇸🇬', '🇲🇾', '🇺🇸']; //设置节点排序方式:索引越小权重越高，空数组时不排序
 module.exports.parse = async (raw, { axios, yaml, notify, console, homeDir }, { name, url, interval, selected }) => {
-  let sortBar = ['🇹🇼', '🇯🇵', '🇸🇬', '🇲🇾', '🇺🇸']; //设置节点排序方式:索引越小权重越高，空数组时不排序
   let obj = yaml.parse(raw);
   // console.log(homeDir)
-  axios.get(homeDir + '\\profiles\\emoji0.json') //使用axios 网络请求框架请求本地文件(遗憾：异步不能处理return yaml.stringify(obj);)
-    .then(function (response) {
-      // console.log(response.data);
-      const emojiObj = response.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  let fileData = fs.readFileSync(homeDir + '\\profiles\\emoji0.json');
-  const emojiObj = JSON.parse(fileData.toString());
-  handleProcess(emojiObj);
-  function handleProcess(emojiObj) {
+  try {
+    //使用axios 网络请求框架请求本地文件
+    // let response = await axios.get(homeDir + '\\profiles\\emoji0.json'); //最外层本身就是async，当然能直接用await
+    let response = await axios.get('https://raw.githubusercontent.com/JayCriss/NetConfig/ForEach/Clash/emoji0.json'); 
+    // console.log(response.data);
+        const emojiObj = response.data;
+    handleProcess(emojiObj, obj);
+    // console.log(yaml.stringify(obj))
+  } catch (error) {
+    console.log(error);
+  }
+
+  // let fileData = fs.readFileSync(homeDir + '\\profiles\\emoji0.json');
+  // const emojiObj = JSON.parse(fileData.toString());
+
+  // setTimeout(() => {}, 1000)
+  // console.log(yaml.stringify(obj))
+  return yaml.stringify(obj);
+
+  function handleProcess(emojiObj, obj) {
     obj.proxies.map((prx_item, prx_index, prx_arry) => {
       let statusBool
       for (let emojkey in emojiObj) {
@@ -33,29 +41,27 @@ module.exports.parse = async (raw, { axios, yaml, notify, console, homeDir }, { 
           break;
         }
       }
-      !statusBool ? prx_item.name = '🏴‍☠️' + ' ' + prx_item.name : {}
+      !statusBool ? prx_item.name = '🏴‍☠️' + ' ' + prx_item.name : {} //没找到旗帜的的节点默认添加🏴‍☠️
     })
     obj.proxies.sort(sortNodes(sortBar));
     obj.proxies.forEach(function (item, index, arry) {
       obj['proxy-groups'][0].proxies[index] = item.name;
     })
-
-    function sortNodes(sN_arry) {
-      return (a, b) => {
-        let a_temp = 0, b_temp = 0;
-        if (sN_arry.length > 0) {
-          sN_arry.forEach(function (item, index, arry) {
-            a.name.includes(item) ? a_temp = sN_arry.length - index : {}
-            b.name.includes(item) ? b_temp = sN_arry.length - index : {}
-          })
-          return b_temp - a_temp
-        }
-        return 0
+  }
+  // 节点排序函数，数组索引越小权重越高，空数组时不排序
+  function sortNodes(sN_arry) {
+    return (a, b) => {
+      let a_temp = 0, b_temp = 0;
+      if (sN_arry.length > 0) {
+        sN_arry.forEach(function (item, index, arry) {
+          a.name.includes(item) ? a_temp = sN_arry.length - index : {}
+          b.name.includes(item) ? b_temp = sN_arry.length - index : {}
+        })
+        return b_temp - a_temp
       }
-
+      return 0
     }
   }
 
-  return yaml.stringify(obj);
 
 }
